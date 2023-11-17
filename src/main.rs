5f6ede88e911868146acb35fb8af39543354ddf1
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-11-17 23:35:19 +0800

    refactor: optionally run pre and post scripts

--- src/main.rs
+++ src/main.rs
@@ -33,117 +33,127 @@ struct Cli {
     /// Apply additional behaviors to emulate a specific package manager
     #[arg(short, long)]
     compat: Option<CompatMode>,
+
+    /// Run pre- and post- scripts
+    #[arg(short, long)]
+    pre_post: bool,
 }
 
-async fn execute(
-    script_name: &str,
-    extra_args: Option<&Vec<String>>,
-    compat_mode: Option<CompatMode>,
-) -> Result<()> {
-    let mut executed_script = false;
-    let mut searched_dirs: Vec<PathBuf> = vec![];
+impl Cli {
+    async fn execute(&self) -> Result<()> {
+        let mut executed_script = false;
+        let mut searched_dirs: Vec<PathBuf> = vec![];
 
-    for search_dir in env::current_dir()?.ancestors() {
-        let this_pkg = search_dir.join("package.json");
+        for search_dir in env::current_dir()?.ancestors() {
+            let this_pkg = search_dir.join("package.json");
 
-        if this_pkg.exists() && this_pkg.is_file() {
-            if let Ok(Ok(package_data)) = fs::read_to_string(&this_pkg)
-                .await
-                .map(|raw| serde_json::from_str::<PackageJson>(&raw))
-            {
-                if let Some(script_cmd) = package_data.scripts.get(script_name) {
-                    let pre_script_name = "pre".to_owned() + script_name;
-                    let post_script_name = "post".to_owned() + script_name;
+            if this_pkg.exists() && this_pkg.is_file() {
+                if let Ok(Ok(package_data)) = fs::read_to_string(&this_pkg)
+                    .await
+                    .map(|raw| serde_json::from_str::<PackageJson>(&raw))
+                {
+                    if let Some(script_cmd) = package_data.scripts.get(&self.script) {
+                        eprint!("{}", make_package_prefix(&package_data));
 
-                    eprint!("{}", make_package_prefix(&package_data));
+                        if self.pre_post {
+                            let pre_script_name = "pre".to_owned() + &self.script;
+                            if let Some(pre_script_cmd) = package_data.scripts.get(&pre_script_name)
+                            {
+                                run_script(
+                                    &this_pkg,
+                                    &package_data,
+                                    &pre_script_name,
+                                    pre_script_cmd,
+                                    None,
+                                    self.compat,
+                                )
+                                .await?;
+                            }
+                        }
 
-                    if let Some(pre_script_cmd) = package_data.scripts.get(&pre_script_name) {
                         run_script(
                             &this_pkg,
                             &package_data,
-                            &pre_script_name,
-                            pre_script_cmd,
-                            None,
-                            compat_mode,
+                            &self.script,
+                            script_cmd,
+                            self.extra_args.as_ref(),
+                            self.compat,
                         )
                         .await?;
+
+                        if self.pre_post {
+                            let post_script_name = "post".to_owned() + &self.script;
+                            if let Some(post_script_cmd) =
+                                package_data.scripts.get(&post_script_name)
+                            {
+                                run_script(
+                                    &this_pkg,
+                                    &package_data,
+                                    &post_script_name,
+                                    post_script_cmd,
+                                    None,
+                                    self.compat,
+                                )
+                                .await?;
+                            }
+                        }
+
+                        executed_script = true;
+                        break;
                     }
-
-                    run_script(
-                        &this_pkg,
-                        &package_data,
-                        script_name,
-                        script_cmd,
-                        extra_args,
-                        compat_mode,
-                    )
-                    .await?;
-
-                    if let Some(post_script_cmd) = package_data.scripts.get(&post_script_name) {
-                        run_script(
-                            &this_pkg,
-                            &package_data,
-                            &post_script_name,
-                            post_script_cmd,
-                            None,
-                            compat_mode,
-                        )
-                        .await?;
-                    }
-
-                    executed_script = true;
-                    break;
                 }
-            }
 
-            searched_dirs.push(this_pkg);
+                searched_dirs.push(this_pkg);
+            }
         }
+
+        if !executed_script {
+            eprintln!(
+                "{}{}{}",
+                "No script found with name ".red(),
+                &self.script.red().bold(),
+                "!".red()
+            );
+
+            eprintln!(
+                "{} {}",
+                "Searched:".dimmed(),
+                if searched_dirs.is_empty() {
+                    "<no packages found>".to_owned()
+                } else {
+                    searched_dirs
+                        .iter()
+                        .map(|p| p.to_string_lossy())
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                        .dimmed()
+                        .to_string()
+                }
+            );
+        }
+
+        Ok(())
     }
-
-    if !executed_script {
-        eprintln!(
-            "{}{}{}",
-            "No script found with name ".red(),
-            &script_name.red().bold(),
-            "!".red()
-        );
-
-        eprintln!(
-            "{} {}",
-            "Searched:".dimmed(),
-            if searched_dirs.is_empty() {
-                "<no packages found>".to_owned()
-            } else {
-                searched_dirs
-                    .iter()
-                    .map(|p| p.to_string_lossy())
-                    .collect::<Vec<_>>()
-                    .join(", ")
-                    .dimmed()
-                    .to_string()
-            }
-        );
-    }
-
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let raw_args: Vec<String> = env::args().collect();
 
-    let cli = if env::var_os("NRR_COMPAT_MODE").is_some_and(|v| !v.is_empty())
-        && raw_args.get(1) == Some(&"run".to_owned())
-    {
-        let mut processed_args = raw_args.clone();
-        processed_args.remove(1);
+    let cli = Cli::parse_from(
+        if env::var_os("NRR_COMPAT_MODE").is_some_and(|v| !v.is_empty())
+            && raw_args.get(1) == Some(&"run".to_owned())
+        {
+            let mut processed_args = raw_args.clone();
+            processed_args.remove(1);
 
-        Cli::parse_from(&processed_args)
-    } else {
-        Cli::parse_from(&raw_args)
-    };
+            processed_args
+        } else {
+            raw_args
+        },
+    );
 
-    execute(&cli.script, cli.extra_args.as_ref(), cli.compat).await?;
+    cli.execute().await?;
 
     Ok(())
 }
